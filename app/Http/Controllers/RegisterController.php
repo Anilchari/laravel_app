6cb0b56a2e152445fffcfb393e7f6ee7421ae64f
anil chari <anilcharisuddhoju@gmail.com>
2023-06-07 22:31:39 +0530

    done

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,14 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\UserModel;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class RegisterController extends Controller
 {
     public function index(Request $request)
     {
         // return view('index');
-        dd($request->all());
+        // dd($request->all());
+        $check_data_exist_r_not = UserModel::where('email','=',$request->email)->first();
+        if(empty($check_data_exist_r_not))
+        {
+            $save_data = DB::table('user')->insert(
+                [
+                    'name'=>$request->name,
+                    'email'=>$request->email,
+                    'password'=>$request->password
+                ]
+            );
+
+            if($save_data)
+            {
+                echo "Data inserted";
+            }
+            else
+            {
+                echo "Data inserted";
+            }
+        }
+        else
+        {
+            echo "User already exist!";
+        }
     }
 }
